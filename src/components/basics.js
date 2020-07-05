df87051d886cf8832ea4d68e5ea58a5e1223640b
Komal Singh <komalsingh2881995@gmail.com>
2020-07-05 23:04:44 +0530

    update

---
+++ src/components/basics.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Basics extends Component {
+    
+    render() { 
+        return (
+            <div>
+                fhjskdhfkj
+            </div>
+          );
+    }
+}
+ 
+export default Basics;
