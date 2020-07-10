b05d3d7857c0dcf6344a83689b9b521ea5bea9de
Komal Singh <komalsingh2881995@gmail.com>
2020-07-10 13:08:54 +0530

    lesson 83

--- src/components/basics.js
+++ src/components/basics.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Child from "./child";
 import basics from "./basics.css"
+import ErrorBoundry from "../ErrorBoundry/ErrorBoundry"
 
 class Basics extends Component {
     state = {
@@ -9,7 +10,7 @@ class Basics extends Component {
             { "name": "sdjkf", "age": "22" },
             { "name": "komalnfdk", "age": "25" }
         ],
-        show:false
+        show: false
     }
 
     applyEvent = () => {
@@ -21,26 +22,35 @@ class Basics extends Component {
                 { "name": "koma........lnfdk", "age": "15" }
             ]
         })
-        this.setState({show:true})
+        this.setState({ show: true })
     }
 
+
+
     render() {
+        // const rnd = Math.random();
+
+        // if (rnd > 0.7) {
+        //     throw new Error("somthing went roung");
+        // }
         return (
-            <div className="container">
-                  <button className="but" onClick={() => this.applyEvent()}>Click Event</button>
-                {
-                    this.state.show ?
-                        <div>
-                            <h1>Hi I am komal</h1>
-                            <p>Hello jiiiii........................</p>
-                          
-                            <Child name={this.state.arr[0].name} age={this.state.arr[0].age} />
-                            <Child name={this.state.arr[1].name} age={this.state.arr[1].age} />
-                            <Child name={this.state.arr[2].name} age={this.state.arr[2].age} />
-                        </div> : null
-                }
-                <div>{this.state.arr.map(p=><Child name={p.name} age={p.age} />)}</div>
-            </div>
+            <ErrorBoundry>
+                <div className="container">
+                    <button className="but" onClick={() => this.applyEvent()}>Click Event</button>
+                    {
+                        this.state.show ?
+                            <div>
+                                <h1>Hi I am komal</h1>
+                                <p>Hello jiiiii........................</p>
+
+                                <Child name={this.state.arr[0].name} age={this.state.arr[0].age} />
+                                <Child name={this.state.arr[1].name} age={this.state.arr[1].age} />
+                                <Child name={this.state.arr[2].name} age={this.state.arr[2].age} />
+                            </div> : null
+                    }
+                    <div>{this.state.arr.map(p => <Child name={p.name} age={p.age} />)}</div>
+                </div>
+            </ErrorBoundry>
         );
     }
 }
